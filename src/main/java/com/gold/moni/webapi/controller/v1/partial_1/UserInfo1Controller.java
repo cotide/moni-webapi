39c7bb5b83b0cde1e2e62148845965e7fe35c2ab
cotide <xcli.code@gmail.com>
2018-09-05 18:48:42 +0800

    🌴  update springboot rest project

--- src/main/java/com/gold/moni/webapi/controller/v1/partial_1/UserInfo1Controller.java
+++ src/main/java/com/gold/moni/webapi/controller/v1/partial_1/UserInfo1Controller.java
@@ -1,4 +1,4 @@
-package com.gold.moni.webapi.controller.v1.part1_1;
+package com.gold.moni.webapi.controller.v1.partial_1;
 
 
 import com.gold.moni.domain.dto.UserInfoDto;
@@ -8,10 +8,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1.1/userInfo")
@@ -20,6 +17,7 @@ public class UserInfo1Controller extends BaseApiController {
 
     @Autowired
     protected UserInfoTask userInfoTask;
+
 
     @ApiOperation(value = "获取用户信息")
     @RequestMapping(value = "getUser",method = RequestMethod.GET)
